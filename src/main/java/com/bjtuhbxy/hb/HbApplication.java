1c1a77955ef910a46931e0680479de7aa6fe80bc
ylt <k74qrlin@gmail.com>
2021-03-08 10:48:45 +0800

    Initial commit

---
+++ src/main/java/com/bjtuhbxy/hb/HbApplication.java
@@ -0,0 +1,13 @@
+package com.bjtuhbxy.hb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HbApplication.class, args);
+    }
+
+}
