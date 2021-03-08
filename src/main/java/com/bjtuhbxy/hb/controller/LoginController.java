cb425e9bf4ed9eeebe69fd8098b6734c84acd43c
ylt <k74qrlin@gmail.com>
2021-03-08 21:56:24 +0800

    删除冲突依赖

--- src/main/java/com/bjtuhbxy/hb/controller/LoginController.java
+++ src/main/java/com/bjtuhbxy/hb/controller/LoginController.java
@@ -2,14 +2,16 @@ package com.bjtuhbxy.hb.controller;
 
 import com.bjtuhbxy.hb.pojo.User;
 import com.bjtuhbxy.hb.result.Result;
+import com.bjtuhbxy.hb.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.HtmlUtils;
 
-import java.util.Objects;
-
 @Controller
 public class LoginController {
+    @Autowired
+    UserService userService;
 
     @CrossOrigin
     @PostMapping(value = "api/login")
@@ -17,17 +19,8 @@ public class LoginController {
     public Result login(@RequestBody User requestUser) {
     // 对 html 标签进行转义，防止 XSS 攻击
         String username = requestUser.getUsername();
-        username = HtmlUtils.htmlEscape(username);
-
-        if (!Objects.equals("admin", username) || !Objects.equals("123456", requestUser.getPassword())) {
-            String message = "账号密码错误";
-            System.out.println("test");
-            return new Result(400);
-        } else {
-            return new Result(200);
-        }
-
-        if (!Objects.equals("admin", username) || !Objects.equals("123456", requestUser.getPassword())) {
+        User user = userService.get(username, requestUser.getPassword());
+        if (null==user) {
             String message = "账号密码错误";
             System.out.println("test");
             return new Result(400);
---
+++ src/main/java/com/bjtuhbxy/hb/dao/UserDAO.java
@@ -0,0 +1,10 @@
+package com.bjtuhbxy.hb.dao;
+
+import com.bjtuhbxy.hb.pojo.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserDAO extends JpaRepository<User,Integer> {
+    User findByUsername(String username);
+
+    User getByUsernameAndPassword(String username,String password);
+}
