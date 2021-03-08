2ca799c4e1b03a84550c3153894deff69bb52f2d
ylt <k74qrlin@gmail.com>
2021-03-08 19:45:48 +0800

    基本框架搭建

---
+++ src/main/java/com/bjtuhbxy/hb/controller/LoginController.java
@@ -0,0 +1,39 @@
+package com.bjtuhbxy.hb.controller;
+
+import com.bjtuhbxy.hb.pojo.User;
+import com.bjtuhbxy.hb.result.Result;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.HtmlUtils;
+
+import java.util.Objects;
+
+@Controller
+public class LoginController {
+
+    @CrossOrigin
+    @PostMapping(value = "api/login")
+    @ResponseBody
+    public Result login(@RequestBody User requestUser) {
+    // 对 html 标签进行转义，防止 XSS 攻击
+        String username = requestUser.getUsername();
+        username = HtmlUtils.htmlEscape(username);
+
+        if (!Objects.equals("admin", username) || !Objects.equals("123456", requestUser.getPassword())) {
+            String message = "账号密码错误";
+            System.out.println("test");
+            return new Result(400);
+        } else {
+            return new Result(200);
+        }
+
+        if (!Objects.equals("admin", username) || !Objects.equals("123456", requestUser.getPassword())) {
+            String message = "账号密码错误";
+            System.out.println("test");
+            return new Result(400);
+        } else {
+            return new Result(200);
+        }
+    }
+}
+
